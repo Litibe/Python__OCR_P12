2be2dd5e80644c7aaea81325588a988094944438
LiTiBe <contact@litiboost.fr>
2022-03-29 18:18:34 +0200

    add event need contract into models/admin

--- crm/admin.py
+++ crm/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from crm.models import Customer
+from crm.models import Customer, Contract, Event, Need
+
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
+
     list_display = [field.name for field in Customer._meta.fields if field.name != "id"]
 
     filter_horizontal = ()
@@ -13,10 +15,46 @@ class CustomerAdmin(admin.ModelAdmin):
             return True
         else:
             return False
+    def has_module_permission(self, request):
+        try:
+            if request.user.profile_staff.customer_read:
+                return True
+            else:
+                return False
+        except AttributeError: 
+            return False 
+    def has_change_permission(self, request, obj=None):
+        if request.user.profile_staff.customer_update:
+            return True
+        else:
+            return False
+    def has_delete_permission(self, request, obj=None):
+        if request.user.profile_staff.customer_delete:
+            return True
+        else:
+            return False
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Contract._meta.fields if field.name != "id"]
+
+    filter_horizontal = ()
+
+    def has_add_permission(self, request):
+        if request.user.profile_staff.contract_create:
+            return True
+        else:
+            return False
     
     def has_module_permission(self, request):
         try: 
-            if request.user.profile_staff.customer_read:
+            if request.user.profile_staff.contract_read:
                 return True
             else:
                 return False
@@ -24,13 +62,92 @@ class CustomerAdmin(admin.ModelAdmin):
             return False
         
     def has_change_permission(self, request, obj=None):
-        if request.user.profile_staff.customer_update:
+        if request.user.profile_staff.contract_update:
             return True
         else:
             return False
        
     def has_delete_permission(self, request, obj=None):
-        if request.user.profile_staff.customer_delete:
+        if request.user.profile_staff.contract_delete:
+            return True
+        else:
+            return False
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions,
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Event._meta.fields if field.name != "id"]
+
+    filter_horizontal = ()
+
+    def has_add_permission(self, request):
+        if request.user.profile_staff.event_create:
+            return True
+        else:
+            return False
+    
+    def has_module_permission(self, request):
+        try: 
+            if request.user.profile_staff.event_read:
+                return True
+            else:
+                return False
+        except AttributeError: 
+            return False
+        
+    def has_change_permission(self, request, obj=None):
+        if request.user.profile_staff.event_update:
+            return True
+        else:
+            return False
+       
+    def has_delete_permission(self, request, obj=None):
+        if request.user.profile_staff.event_delete:
+            return True
+        else:
+            return False
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+@admin.register(Need)
+class NeedAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Need._meta.fields if field.name != "id"]
+
+    filter_horizontal = ()
+
+    def has_add_permission(self, request):
+        if request.user.profile_staff.need_create:
+            return True
+        else:
+            return False
+    
+    def has_module_permission(self, request):
+        try: 
+            if request.user.profile_staff.need_read:
+                return True
+            else:
+                return False
+        except AttributeError: 
+            return False
+        
+    def has_change_permission(self, request, obj=None):
+        if request.user.profile_staff.need_update:
+            return True
+        else:
+            return False
+       
+    def has_delete_permission(self, request, obj=None):
+        if request.user.profile_staff.need_delete:
             return True
         else:
             return False
--- crm/models.py
+++ crm/models.py
@@ -17,4 +17,47 @@ class Customer(models.Model):
     date_created = models.DateTimeField(verbose_name="Date Created")
     date_updated = models.DateTimeField(verbose_name="Date Updated")
     sales_contact = models.ForeignKey(
-        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        to=settings.AUTH_USER_MODEL,
+        limit_choices_to={'profile_staff': 'SALES'},
+        on_delete=models.CASCADE)
+
+
+class Contract(models.Model):
+    """ 
+    Class Object Contract for Customer
+    """
+    date_created = models.DateTimeField(verbose_name="Date Start Contract")
+    date_finished = models.DateTimeField(verbose_name="Date End Contract")
+    signed = models.BooleanField('signed', default=False)
+    customer_assigned = models.ForeignKey(
+        to=Customer, on_delete=models.CASCADE)
+
+
+class Event(models.Model):
+    """ 
+    Class Object Event
+    """
+    title = models.CharField(max_length=125, verbose_name="Title Event")
+    date_created = models.DateTimeField(verbose_name="Date Start")
+    date_updated = models.DateTimeField(verbose_name="Date Updated")
+    date_finished = models.DateTimeField(verbose_name="Date End")
+    support_contact = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        limit_choices_to={'profile_staff': 'SUPPORT'},
+        on_delete=models.CASCADE)
+    contract_assigned = models.ForeignKey(
+        to=Contract, on_delete=models.CASCADE)
+
+class Need(models.Model):
+    """ 
+    Class Object Need For Event
+    """
+    title = models.CharField(max_length=125, verbose_name="title Need")
+    success = models.BooleanField('signed', default=False)
+    date_updated = models.DateTimeField(verbose_name="Date Updated")
+    support_contact = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        limit_choices_to={'profile_staff': 'SUPPORT'},
+        on_delete=models.CASCADE)
+    event_assigned = models.ForeignKey(
+        to=Event, on_delete=models.CASCADE)
