1d60b4fe95fca72f49c21613bd0147f6b7d4e7f5
LiTiBe <contact@litiboost.fr>
2022-03-31 13:00:15 +0200

    add tests model customer

--- crm/models.py
+++ crm/models.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from django.utils import timezone
 from django.conf import settings
 from django.db import models
 
@@ -13,9 +13,12 @@ class Customer(models.Model):
         max_length=100, verbose_name="email", unique=True)
     phone = models.CharField(max_length=20, verbose_name="Phone Number")
     mobile = models.CharField(max_length=20, verbose_name="Mobile Number")
-    company_name = models.CharField(max_length=250, verbose_name="Compagny Name")
-    date_created = models.DateTimeField(verbose_name="Date Created")
-    date_updated = models.DateTimeField(verbose_name="Date Updated")
+    company_name = models.CharField(max_length=250, 
+                                    verbose_name="Compagny Name")
+    date_created = models.DateTimeField(
+        verbose_name="Date Created", default=timezone.now)
+    date_updated = models.DateTimeField(
+        verbose_name="Date Updated", default=timezone.now)
     sales_contact = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         limit_choices_to={'profile_staff': 2},
@@ -23,23 +26,27 @@ class Customer(models.Model):
 
 
 class Contract(models.Model):
-    """ 
+    """
     Class Object Contract for Customer
     """
-    date_created = models.DateTimeField(verbose_name="Date Start Contract")
-    date_finished = models.DateTimeField(verbose_name="Date End Contract")
+    date_created = models.DateTimeField(
+        verbose_name="Date Start Contract", default=timezone.now)
+    date_finished = models.DateTimeField(
+        verbose_name="Date End Contract", default=timezone.now)
     signed = models.BooleanField('signed', default=False)
     customer_assigned = models.ForeignKey(
         to=Customer, on_delete=models.CASCADE)
 
 
 class Event(models.Model):
-    """ 
+    """
     Class Object Event
     """
     title = models.CharField(max_length=125, verbose_name="Title Event")
-    date_created = models.DateTimeField(verbose_name="Date Start")
-    date_updated = models.DateTimeField(verbose_name="Date Updated")
+    date_created = models.DateTimeField(
+        verbose_name="Date Start", default=timezone.now)
+    date_updated = models.DateTimeField(
+        verbose_name="Date Updated", default=timezone.now)
     date_finished = models.DateTimeField(verbose_name="Date End")
     support_contact = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
@@ -48,12 +55,14 @@ class Event(models.Model):
     contract_assigned = models.ForeignKey(
         to=Contract, on_delete=models.CASCADE)
 
+
 class Need(models.Model):
-    """ 
+    """
     Class Object Need For Event
     """
     title = models.CharField(max_length=125, verbose_name="title Need")
     success = models.BooleanField('signed', default=False)
-    date_updated = models.DateTimeField(verbose_name="Date Updated")
+    date_updated = models.DateTimeField(
+        verbose_name="Date Updated", default=timezone.now)
     event_assigned = models.ForeignKey(
         to=Event, on_delete=models.CASCADE)
--- crm/tests.py
+++ crm/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from authentication.models import ProfileStaff, User
 
-# Create your tests here.
+from crm.models import Customer
+
+
+def create_new_user_support():
+    profile_support = ProfileStaff.objects.create(name="SUPPORT")
+    profile_support.save()
+    profile_staff = ProfileStaff.objects.filter(name="SUPPORT").first()
+    new_user = User.objects.create(
+        email="support@epicevents.fr",
+        first_name="prenom",
+        last_name='nom',
+        profile_staff=profile_staff,
+    )
+    new_user.set_password("epicevents")
+    new_user.save()
+
+
+def create_new_user_sales():
+    profile_sales = ProfileStaff.objects.create(name="SALES")
+    profile_sales.save()
+    profile_staff = ProfileStaff.objects.filter(name="SALES").first()
+    new_user = User.objects.create(
+        email="sales@epicevents.fr",
+        first_name="prenom",
+        last_name='nom',
+        profile_staff=profile_staff,
+    )
+    new_user.set_password("epicevents")
+    new_user.save()
+
+
+class TestUnitaireModels(TestCase):
+    def test_create_new_customer(self):
+        create_new_user_sales()
+        profile_staff = ProfileStaff.objects.filter(name="SALES").first()
+
+        user_sales = User.objects.filter(
+            profile_staff=profile_staff).first()
+        new_customer = Customer.objects.create(
+            first_name="Tony",
+            last_name="DURAND",
+            email="tonydurant@entreprise.com",
+            phone="0987654321",
+            mobile="067899887",
+            company_name="Entreprise SAS",
+            sales_contact=user_sales
+        )
+        new_customer.save()
+        db_customer = Customer.objects.all().first()
+        assert db_customer.id == 1
+        assert db_customer.first_name == "Tony"
+        assert db_customer.sales_contact.profile_staff.name == "SALES"
