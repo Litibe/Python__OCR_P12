89b91fcd2596e05a8518ca22a807795445a02bcc
LiTiBe <contact@litiboost.fr>
2022-04-05 11:45:03 +0200

    modify ID diffrent model class

--- crm/models.py
+++ crm/models.py
@@ -7,6 +7,7 @@ class Customer(models.Model):
     """
     Class Object Customer
     """
+    id = models.CharField(primary_key=True, unique=True, max_length=8)
     first_name = models.CharField(max_length=25, verbose_name="First Name")
     last_name = models.CharField(max_length=25, verbose_name="Last Name")
     email = models.EmailField(
@@ -16,40 +17,62 @@ class Customer(models.Model):
     company_name = models.CharField(max_length=250, 
                                     verbose_name="Compagny Name")
     date_created = models.DateTimeField(
-        verbose_name="Date Created", default=timezone.now)
+        verbose_name="Date Created", auto_now_add=True)
     date_updated = models.DateTimeField(
-        verbose_name="Date Updated", default=timezone.now)
+        verbose_name="Date Updated", auto_now_add=True)
     sales_contact = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         limit_choices_to={'profile_staff': 2},
         on_delete=models.CASCADE)
+    
+    def save(self, *args, **kwargs):
+        """Save Method surcharged to update ID Customer CMXXXXX
+        """
+        last_customer = Customer.objects.all().order_by('id').last()
+        if not last_customer:
+            self.id = "CM00001"
+        else:
+            self.id = "CM" + str(int(last_customer.id[2:])+1)
+        super().save(*args, **kwargs)
 
 
 class Contract(models.Model):
     """
     Class Object Contract for Customer
     """
+    id = models.CharField(primary_key=True, unique=True, max_length=8)
     title = models.CharField(max_length=125,
                              verbose_name="Title Contract",
-                             default="Titlte Contract")
+                             default="Title Contract")
     date_start_contract = models.DateTimeField(
-        verbose_name="Date Start Contract", default=timezone.now)
+        verbose_name="Date Start Contract", auto_now_add=True)
     date_end_contract = models.DateTimeField(
         verbose_name="Date End Contract")
-    signed = models.BooleanField('signed', default=False)
+    signed = models.BooleanField(verbose_name='signed', default=False)
     customer_assigned = models.ForeignKey(
         to=Customer, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        """Save Method surcharged to update ID Contract CTXXXXX
+        """
+        last_contract = Contract.objects.all().order_by('id').last()
+        if not last_contract:
+            self.id = "CT00001"
+        else:
+            self.id = "CT" + str(int(last_contract.id[2:])+1)
+        super().save(*args, **kwargs)
 
 
 class Event(models.Model):
     """
     Class Object Event
     """
+    id = models.CharField(primary_key=True, unique=True, max_length=8)
     title = models.CharField(max_length=125, verbose_name="Title Event")
     date_created = models.DateTimeField(
-        verbose_name="Date Start", default=timezone.now)
+        verbose_name="Date Start", auto_now_add=True)
     date_updated = models.DateTimeField(
-        verbose_name="Date Updated", default=timezone.now)
+        verbose_name="Date Updated", auto_now_add=True)
     date_finished = models.DateTimeField(verbose_name="Date End")
     support_contact = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
@@ -58,14 +81,35 @@ class Event(models.Model):
     contract_assigned = models.ForeignKey(
         to=Contract, on_delete=models.CASCADE)
 
+    def save(self, *args, **kwargs):
+        """Save Method surcharged to update ID Event EXXXXX
+        """
+        last_event = Event.objects.all().order_by('id').last()
+        if not last_event:
+            self.id = "E00001"
+        else:
+            self.id = "E" + str(int(last_event.id[1:])+1)
+        super().save(*args, **kwargs)
+
 
 class Need(models.Model):
     """
     Class Object Need For Event
     """
+    id = models.CharField(primary_key=True, unique=True, max_length=8)
     title = models.CharField(max_length=125, verbose_name="title Need")
-    success = models.BooleanField('signed', default=False)
+    success = models.BooleanField(verbose_name='success', default=False)
     date_updated = models.DateTimeField(
-        verbose_name="Date Updated", default=timezone.now)
+        verbose_name="Date Updated", auto_now_add=True)
     event_assigned = models.ForeignKey(
         to=Event, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        """Save Method surcharged to update ID Need NXXXXX
+        """
+        last_need = Need.objects.all().order_by('id').last()
+        if not last_need:
+            self.id = "N00001"
+        else:
+            self.id = "N" + str(int(last_need.id[1:])+1)
+        super().save(*args, **kwargs)
--- crm/tests.py
+++ crm/tests.py
@@ -1,3 +1,4 @@
+import datetime
 from django.test import TestCase
 from authentication.models import ProfileStaff, User
 
@@ -5,6 +6,11 @@ from crm.models import Contract, Customer
 
 
 def create_new_user_support():
+    """Create New User with profile_staff.name==SUPPORT into DB TEST
+
+    Returns:
+        User Object with Profile_staff.id=3 created after search into db
+    """
     profile_support = ProfileStaff.objects.create(name="SUPPORT")
     profile_support.save()
     profile_staff = ProfileStaff.objects.filter(name="SUPPORT").first()
@@ -19,6 +25,11 @@ def create_new_user_support():
 
 
 def create_new_user_sales():
+    """Create New User with profile_staff.name==SALES into DB TEST
+
+    Returns:
+        User Object with Profile_staff.id=2 created after search into db
+    """
     profile_sales = ProfileStaff.objects.create(name="SALES")
     profile_sales.save()
     profile_staff = ProfileStaff.objects.filter(name="SALES").first()
@@ -36,6 +47,14 @@ def create_new_user_sales():
 
 
 def create_new_customer(user_sales):
+    """Create New Customer into DB TEST
+
+    Args:
+        user_sales (User Object with Profile_staff.id=2)
+
+    Returns:
+        Customer Object created after search into db
+    """
     new_customer = Customer.objects.create(
             first_name="Tony",
             last_name="DURAND",
@@ -49,22 +68,48 @@ def create_new_customer(user_sales):
     search_customer = Customer.objects.all().first()
     return search_customer
 
+
 def create_new_contract(search_customer):
+    """Create New Contract into DB TEST
+
+    Args:
+        search_customer (Customer Object)
+
+    Returns:
+        Contract Object created after search into db
+    """
+    d_start = "2022-06-03T08:00:00+02:00"
+    d_end = "2022-06-05T20:00:00+02:00"
     new_contract = Contract.objects.create(
-        
+        title="Spring 2022 - Paris",
+        date_start_contract=d_start,
+        date_end_contract=d_end,
+        signed=True,
+        customer_assigned=search_customer
     )
+    new_contract.save()
+    search_contract = Contract.objects.filter(
+                        title="Spring 2022 - Paris").first()
+    return search_contract
+
 
 class TestUnitaireModels(TestCase):
-    def test_create_new_customer(self):
-        user_sales = create_new_user_sales()
-        db_customer = create_new_customer(user_sales)
-        assert db_customer.id == 1
-        assert db_customer.first_name == "Tony"
-        assert db_customer.sales_contact.profile_staff.name == "SALES"
+    """ Class TEST Unitaire for Models CRM """
+    def test_01_create_new_customer(self):
+        """create new user_sales into db and new customer with sales_contact
+        """
+        self.user_sales = create_new_user_sales()
+        self.db_customer = create_new_customer(self.user_sales)
+        assert self.db_customer.id == 1
+        assert self.db_customer.first_name == "Tony"
+        assert self.db_customer.sales_contact.profile_staff.name == "SALES"
 
-    def test_create_new_contract(self):
-        user_sales = create_new_user_sales()
-        db_customer = create_new_customer(user_sales)
-        assert db_customer.id == 1
-
-
+    def test_02_create_new_contract(self):
+        """create new contract for a customer
+        """
+        self.user_sales = create_new_user_sales()
+        self.db_customer = create_new_customer(self.user_sales)
+        assert self.db_customer.id == 2
+        self.db_contract = create_new_contract(self.db_customer)
+        assert self.db_contract.customer_assigned == self.db_customer
+        assert self.db_contract.id == "CT00001"
