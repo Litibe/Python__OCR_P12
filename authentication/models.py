2018608afba0835b73910dae63f83e1e68b04e23
LiTiBe <contact@litiboost.fr>
2022-05-22 11:25:34 +0200

    rename name authorization profile_staff

--- authentication/models.py
+++ authentication/models.py
@@ -7,36 +7,44 @@ class ProfileStaff(models.Model):
     name = models.CharField(max_length=30,
                             verbose_name='Name Profile')
     manage_staff_user_crud = models.BooleanField(
-        verbose_name='Manage Staff - CRUD method for User', default=False)
+        verbose_name=(
+            'Manage Staff - CRUD method for User & Profile_staff'
+            ), default=False)
     customer_read = models.BooleanField(
         verbose_name='Customer - Read', default=False)
     customer_CRU_assigned = models.BooleanField(
-        verbose_name='Customer - CRU method for assigned user', default=False)
+        verbose_name=(
+            'Customer - CRU method for Customer.sales_contact assigned'
+            ), default=False)
     customer_CRUD_all = models.BooleanField(
-        verbose_name='Customer - CRUD method for all ', default=False)
+        verbose_name='Customer - CRUD ', default=False)
     contract_read = models.BooleanField(
-        verbose_name='Contract - Read', default=False)
+        verbose_name='Contract - Read only', default=False)
     contract_CRU_assigned = models.BooleanField(
-        verbose_name='Contract - CRU method for assigned user', default=False)
+        verbose_name=(
+            'Contract - CRU method for Customer.sales_contact assigned'
+            ), default=False)
     contract_CRUD_all = models.BooleanField(
         verbose_name='Contract - CRUD method for all', default=False)
     event_read = models.BooleanField(
-        verbose_name='Event - Read', default=False)
+        verbose_name='Event - Read only', default=False)
     event_CRU_assigned = models.BooleanField(
-        verbose_name='Event - CRU method for assigned user', default=False)
+        verbose_name=(
+            'Event - CRU method for Event.support_contact assigned'
+            ), default=False)
     event_CRUD_all = models.BooleanField(
         verbose_name='Event - CRUD method for all', default=False)
     need_read = models.BooleanField(
-        verbose_name='Need - Read', default=False)
+        verbose_name='Need - Read only', default=False)
     need_CRU_assigned = models.BooleanField(
-        verbose_name='Need - CRU method for assigned user', default=False)
+        verbose_name=(
+            'Need - CRU method for Event.support_contact assigned'
+            ), default=False)
     need_CRUD_all = models.BooleanField(
-        verbose_name='Need - CRUD method for all', default=False)
+        verbose_name='Need - CRUD method', default=False)
 
     def __str__(self):
         return f'{self.name}'
-
-    
 
 
 class User(AbstractUser):
